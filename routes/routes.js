0a06089e278d92f1a5b0d345fdb0844425d533b5
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-07-18 21:15:43 +0200

    build: :truck: aggiunge update e delete
    aggiunge updaate e delete project

--- routes/routes.js
+++ routes/routes.js
@@ -39,3 +39,36 @@ router.post("/", async (req, res) => {
     res.status(400).json({ message: err.message });
   }
 });
+
+// Update project
+router.put("/:id", async (req, res) => {
+  try {
+    const project = await Project.findById(req.params.id);
+    if (!project) return res.status(404).json({ message: "Project not found" });
+
+    project.name = req.body.name || project.name;
+    project.link = req.body.link || project.link;
+    project.image = req.body.image || project.image;
+    project.technologies = req.body.technologies || project.technologies;
+    project.description = req.body.description || project.description;
+
+    const updateProject = await project.save();
+    res.json(updatedProject);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+// Delete project
+router.delete("/:id", async (req, res) => {
+  try {
+    const project = await Project.findById(req.params.id);
+    if (!project) return res.status(404).json({ message: "Project not found" });
+    await project.remove();
+    res.json({ message: "Project deleted" });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+module.exports = router;

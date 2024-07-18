8802449a7818d3014a1e25d110021f5e7a501bdf
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-07-18 23:23:44 +0200

    build: :construction: fix bug
    riscrive il codice mancante dal revert del commit

--- server.js
+++ server.js
@@ -1,24 +1,32 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Middleware
 app.use(bodyParser.json());
 
 // MongoDB connection
-mongoose.connect("mongodb://localhost:27017/portfolio", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+const uri = `mongodb+srv://${process.env.MONGODB_USER_PASS}@cluster0.u0afaln.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+mongoose
+.connect(uri)
+.then(() => console.log("Connected to MongoDB yeah :)"))
+.catch((err) => console.error("Connection failed :(", err));
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
-  console.log("Connected to MongoDB");
+    console.log("Connected to MongoDB");
 });
+
+// Routes
+const projectsRouter = require("./routes/routes");
+app.use("/projects", projectsRouter);
 
 // Start server
 app.listen(port, () => {

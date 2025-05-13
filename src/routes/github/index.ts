807c3a99699ac75760e0eb7b2b0770e05cd36476
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-05-13 16:04:06 +0200

    ✨feat: aggiunge la rotta per la sincronizzazione dei repository in index.ts
    - Importata la funzione syncRepos e configurata la rotta PUT /sync per gestire le richieste di sincronizzazione dei repository.

--- src/routes/github/index.ts
+++ src/routes/github/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { getRepos } from './getRepos';
+import { syncRepos } from './sync';
 
 const router = Router();
 
 router.get('/repos', getRepos);
+router.put('/sync', syncRepos as any);
 
 export default router;

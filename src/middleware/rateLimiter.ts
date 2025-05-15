3f0af68c363b2a8da25fc33c0a7e329eeaa5e7d4
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-05-15 17:21:09 +0200

    ✨feat: aggiorna il middleware rate limiter per utilizzare variabili d'ambiente
    - Modificata la configurazione del rate limiter per utilizzare 'rateLimitWindow' e 'rateLimitMax' definiti in 'env', migliorando la flessibilità nella gestione delle richieste.

--- src/middleware/rateLimiter.ts
+++ src/middleware/rateLimiter.ts
@@ -1,6 +1,9 @@
 import rateLimit from 'express-rate-limit';
+import { env } from '../config/env';
 
 export const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minuti
-  max: 100, // Limite di 100 richieste per finestra
+  windowMs: env.rateLimitWindow
+    ? parseInt(env.rateLimitWindow)
+    : 15 * 60 * 1000, // 15 minuti
+  max: env.rateLimitMax ? parseInt(env.rateLimitMax) : 100, // Limite di 100 richieste per finestra
 });

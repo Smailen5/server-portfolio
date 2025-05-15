307aca5af03428fa0e28e0c4ce7c7c62f9350eda
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-05-15 12:48:51 +0200

    ✨feat: migliora la configurazione di CORS per il server
    - Aggiornata la configurazione del middleware CORS per consentire richieste solo da origini specifiche e per gestire i metodi e le intestazioni consentite.

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,13 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: ['https://smailenvargas.com', 'http://localhost:3000'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'x-auth-token'],
+  })
+);
 app.use(bodyParser.json());
 
 // Routes

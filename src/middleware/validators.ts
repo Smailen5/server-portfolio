e6917a53092415c2b32bb821a655232f55a85e90
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-05-15 15:45:15 +0200

    ✨feat: aggiunge validatori per la creazione e l'aggiornamento dei progetti
    - Implementati i validatori per la creazione e l'aggiornamento dei progetti utilizzando 'express-validator'.
    - Aggiunti controlli per i campi 'name', 'link', 'image', 'technologies' e 'description' per garantire la validità dei dati in ingresso.

---
+++ src/middleware/validators.ts
@@ -0,0 +1,55 @@
+import { body, param } from 'express-validator';
+
+// Validazione per la creazione di un progetto
+export const createProjectValidator = [
+  body('name')
+    .notEmpty()
+    .withMessage('Il nome e obbligatorio')
+    .isString()
+    .withMessage('Il nome deve essere una stringa'),
+  body('link')
+    .notEmpty()
+    .withMessage('Il link e obbligatorio')
+    .isURL()
+    .withMessage('il link deve essere un URL valido'),
+  body('image')
+    .notEmpty()
+    .withMessage("L'immagine e obbligatoria")
+    .isString()
+    .withMessage("L'immagine deve essere una stringa"),
+  body('technologies')
+    .isArray()
+    .withMessage('Le tecnologie devono essere un array')
+    .notEmpty()
+    .withMessage('Almeno una tecnologia e obbligatoria'),
+  body('description')
+    .notEmpty()
+    .withMessage('La descrizione e obbligatoria')
+    .isString()
+    .withMessage('La descrizione deve essere una stringa'),
+];
+
+// Validazione per l'aggiornamento di un progetto
+export const updateProjectValidator = [
+  param('id').isInt().withMessage('ID non valido'),
+  body('name')
+    .optional()
+    .isString()
+    .withMessage('Il nome deve essere una stringa'),
+  body('link')
+    .optional()
+    .isURL()
+    .withMessage('Il link deve essere un URL valido'),
+  body('image')
+    .optional()
+    .isURL()
+    .withMessage("L'immagine deve essere un URL valido"),
+  body('technologies')
+    .optional()
+    .isArray()
+    .withMessage('Le tecnologie devono esser un array'),
+  body('description')
+    .optional()
+    .isString()
+    .withMessage('La descrizione deve essere una stringa'),
+];

ab2d31cbdf43459efc2e9534a4337552f78254a3
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-05-20 14:58:02 +0200

    ✨feat: aggiunge middleware di autenticazione per la validazione della API Key
    - Creato il file 'auth.ts' per implementare un middleware che verifica la presenza e la validità della API Key nelle intestazioni delle richieste, migliorando la sicurezza dell'applicazione.

---
+++ src/middleware/auth.ts
@@ -0,0 +1,21 @@
+import { NextFunction, Request, Response } from 'express';
+import { env } from '../config/env';
+import { AppError } from './errorHandler';
+
+export const authMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const apiKey = req.headers['x-api-key'] as string;
+
+  if (!apiKey) {
+    throw new AppError('API Key mancante', 401);
+  }
+
+  if (apiKey !== env.apiKey) {
+    throw new AppError('API Key non valida', 401);
+  }
+
+  next();
+};

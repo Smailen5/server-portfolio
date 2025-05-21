a2f59739037472bfe6bcc379dfaa40afb6848647
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-05-21 12:09:57 +0200

    ✨feat: implementa il controller per l'autenticazione degli utenti
    - Aggiunta la funzione 'logUser' nel file 'authController.ts' per gestire il login degli utenti, con verifica delle credenziali e restituzione di un token JWT.
    - Inclusi controlli per la validità dell'email e della password, e aggiornamento del campo 'lastLogin' per migliorare la sicurezza e l'esperienza utente.

---
+++ src/controllers/users/authController.ts
@@ -0,0 +1,42 @@
+import bcrypt from 'bcrypt';
+import dotenv from 'dotenv';
+import { Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
+import User from '../../models/User';
+
+dotenv.config();
+
+export const logUser = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      res.status(401).json({ message: 'Utente non trovato' });
+      return;
+    }
+
+    const isPasswordValid = await bcrypt.compare(
+      password,
+      user.getDataValue('password')
+    );
+    if (!isPasswordValid) {
+      res.status(401).json({ message: 'Password non valida' });
+      return;
+    }
+
+    // Aggiorna lastLogin
+    await user.update({ lastLogin: new Date() });
+
+    const token = jwt.sign(
+      { id: user.getDataValue('id') },
+      process.env.JWT_SECRET as string,
+      {
+        expiresIn: '24h',
+      }
+    );
+    res.json({ token });
+  } catch (error) {
+    res.status(401).json({ message: 'Credenziali non valide' });
+  }
+};

a162dcd96973418547b1ed45785a22af0064848e
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-05-21 12:15:16 +0200

    ✨feat: aggiunge un limite di richieste per il login
    - Implementato un middleware 'loginLimiter' nel file 'rateLimiter.ts' per limitare a 5 le richieste di accesso ogni 15 minuti, migliorando la sicurezza contro attacchi di forza bruta.
    - Aggiunto un messaggio di errore personalizzato per informare gli utenti in caso di superamento del limite di accesso.

--- src/middleware/rateLimiter.ts
+++ src/middleware/rateLimiter.ts
@@ -7,3 +7,14 @@ export const limiter = rateLimit({
     : 15 * 60 * 1000, // 15 minuti
   max: env.rateLimitMax ? parseInt(env.rateLimitMax) : 100, // Limite di 100 richieste per finestra
 });
+
+export const loginLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 5, // Limite di 5 richieste per finestra
+  message: {
+    success: false,
+    message: 'Troppi tentativi di accesso. Riprova più tardi.',
+  },
+  standardHeaders: true,
+  legacyHeaders: false,
+});

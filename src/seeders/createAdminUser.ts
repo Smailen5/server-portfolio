c4242eb1c97019457043f79b5dc051f3ae910f96
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-05-21 13:28:53 +0200

    ✨feat: aggiorna lo script per la creazione dell'utente amministratore
    - Modificato 'createAdminUser.ts' per utilizzare le credenziali dell'amministratore definite nel file .env, migliorando la sicurezza e la configurabilità. Aggiunta una verifica per garantire che le credenziali siano configurate prima della creazione dell'utente.

--- src/seeders/createAdminUser.ts
+++ src/seeders/createAdminUser.ts
@@ -1,17 +1,22 @@
 import bcrypt from 'bcrypt';
 import { appLogger } from '../config/appLogger';
-import User from '../models/User';
 import sequelize from '../config/database';
+import { env } from '../config/env';
+import User from '../models/User';
 
 const createAdminUser = async () => {
   try {
     await sequelize.sync();
     appLogger.info('Database sincronizzato');
 
-    const hashedPassword = await bcrypt.hash('tua_password', 10);
+    if (!env.adminEmail || !env.adminPassword) {
+      throw new Error('Credenziali admin non configurate nel file .env');
+    }
+
+    const hashedPassword = await bcrypt.hash(env.adminPassword, 10);
 
     await User.create({
-      email: 'tua_email@example.com',
+      email: env.adminEmail,
       password: hashedPassword,
       role: 'admin',
       isActive: true,

0ac2caa1dbd6c44243a1a1257166b63897346d13
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-05-21 12:35:35 +0200

    ✨feat: aggiunge middleware di autenticazione JWT
    - Implementato il middleware 'jwtAuth' nel file 'jwtAuth.ts' per gestire l'autenticazione degli utenti tramite token JWT. Include logging per tentativi di accesso senza token e per errori di verifica del token, migliorando la sicurezza e la tracciabilità nel processo di autenticazione.

---
+++ src/middleware/auth/jwtAuth.ts
@@ -0,0 +1,32 @@
+import { NextFunction, Request, Response } from 'express';
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import { appLogger } from '../../config/appLogger';
+
+interface AuthRequest extends Request {
+  user?: JwtPayload;
+}
+
+export const jwtAuth = (
+  req: AuthRequest,
+  res: Response,
+  next: NextFunction
+): void => {
+  try {
+    const token = req.headers.authorization?.split(' ')[1];
+
+    if (!token) {
+      appLogger.warn('Tentativo di accesso senza token');
+      res
+        .status(401)
+        .json({ message: 'Accesso non autorizzato, token mancante' });
+      return;
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as JwtPayload;
+    req.user = decoded;
+    next();
+  } catch (error) {
+    appLogger.error(`Errore di verifica token: ${error}`);
+    res.status(401).json({ message: 'Token non valido' });
+  }
+};

6fddfe77728b01761779cb9eabf80a532042f13c
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-05-21 13:20:39 +0200

    ✨feat: aggiunge le rotte principali per la gestione di progetti e integrazione con GitHub
    - Creato il file 'index.ts' per definire le rotte relative ai progetti, GitHub e utenti. Include operazioni CRUD per i progetti e la sincronizzazione dei repository GitHub, migliorando l'organizzazione delle rotte nel progetto.

---
+++ src/routes/index.ts
@@ -0,0 +1,27 @@
+import { Router } from 'express';
+import { getRepos } from './github/getRepos';
+import { syncRepos } from './github/sync';
+import { deleteProject } from './projects/delete/deleteProject';
+import { getAllProjects } from './projects/get/getAllProjects';
+import { getProjectById } from './projects/get/getProjectById';
+import { createProject } from './projects/post/createProject';
+import { updateProject } from './projects/put/updateProject';
+import usersRoutes from './users/routes';
+
+const router = Router();
+
+// Rotte Projects
+router.get('/projects', getAllProjects);
+router.get('/projects/:id', getProjectById);
+router.post('/projects', createProject);
+router.put('/projects/:id', updateProject);
+router.delete('/projects/:id', deleteProject);
+
+// Rotte GitHub
+router.get('/github/repos', getRepos);
+router.put('/github/sync', ...syncRepos);
+
+// Rotte Users
+router.use('/users', usersRoutes);
+
+export default router;

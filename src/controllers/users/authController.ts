a025d0e138b863a5dc95a16ade1d56229c254408
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-05-21 13:59:34 +0200

    ✨feat: migliora la gestione degli errori e le risposte nel login degli utenti
    - Aggiornato il file 'authController.ts' per fornire risposte più dettagliate in caso di login fallito, includendo messaggi di errore più chiari e una struttura di risposta uniforme. Aggiunto logging per errori generali durante il processo di login, migliorando la tracciabilità e la gestione degli errori.

--- src/controllers/users/authController.ts
+++ src/controllers/users/authController.ts
@@ -1,30 +1,40 @@
 import bcrypt from 'bcrypt';
-import dotenv from 'dotenv';
 import { Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
-import User from '../../models/User';
 import { appLogger } from '../../config/appLogger';
-
-dotenv.config();
+import User from '../../models/User';
 
 export const logUser = async (req: Request, res: Response): Promise<void> => {
   try {
     const { email, password } = req.body;
+
     const user = await User.findOne({ where: { email } });
 
     if (!user) {
-      appLogger.warn(`Tentativo di login fallito: email non trovata - ${email}`);
-      res.status(401).json({ message: 'Utente non trovato' });
+      appLogger.warn(
+        `Tentativo di login fallito: utente non trovato - ${email}`
+      );
+      res.status(401).json({
+        success: false,
+        message: 'Credenziali non valide',
+      });
       return;
     }
 
+    // Verifica la password con bcrypt
     const isPasswordValid = await bcrypt.compare(
       password,
       user.getDataValue('password')
     );
+
     if (!isPasswordValid) {
-      appLogger.warn(`Tentativo di login fallito: password non valida - ${email}`);
-      res.status(401).json({ message: 'Password non valida' });
+      appLogger.warn(
+        `Tentativo di login fallito: password non valida - ${email}`
+      );
+      res.status(401).json({
+        success: false,
+        message: 'Credenziali non valide',
+      });
       return;
     }
 
@@ -34,14 +44,16 @@ export const logUser = async (req: Request, res: Response): Promise<void> => {
     const token = jwt.sign(
       { id: user.getDataValue('id') },
       process.env.JWT_SECRET as string,
-      {
-        expiresIn: '24h',
-      }
+      { expiresIn: '24h' }
     );
+
     appLogger.info(`Login effettuato con successo: ${email}`);
     res.json({ token });
   } catch (error) {
-    appLogger.error(`Errore durante il login: ${error}`)
-    res.status(401).json({ message: 'Credenziali non valide' });
+    appLogger.error(`Errore durante il login: ${error}`);
+    res.status(500).json({
+      success: false,
+      message: 'Errore durante il login',
+    });
   }
 };

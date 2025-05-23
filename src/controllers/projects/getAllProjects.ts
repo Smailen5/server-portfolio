e64900d0bfffa16750eb52887c17f5c3dc79c6e9
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-05-23 14:57:43 +0200

    feat: ✨ ordina i progetti per data di creazione
    - Aggiornata la funzione getAllProjects per restituire i progetti ordinati in base alla data di creazione.

--- src/controllers/projects/getAllProjects.ts
+++ src/controllers/projects/getAllProjects.ts
@@ -3,7 +3,9 @@ import Project from '../../models/Project';
 
 export const getAllProjects = (async (_req: Request, res: Response) => {
   try {
-    const projects = await Project.findAll();
+    const projects = await Project.findAll({
+      order: [['createdAt', 'DESC']],
+    });
     return res.json(projects);
   } catch (err: any) {
     return res.status(500).json({ message: err.message });

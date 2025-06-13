f8edbc03a2496c3e17eeba1b25d156c902d69b05
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-06-13 19:42:51 +0200

    fix: 🐛 migliora la gestione degli errori
    - Aggiunto il logging per il recupero dei dati GitHub, con gestione degli errori tramite appLogger.
    - Modificato il messaggio di errore durante la sincronizzazione del database per utilizzare appLogger.

--- src/config/initDb.ts
+++ src/config/initDb.ts
@@ -1,4 +1,6 @@
+import { getRepos } from '../controllers/github/getRepos';
 import { createAdminUser } from '../seeders/createAdminUser';
+import { appLogger } from './appLogger';
 import sequelize from './database';
 
 export const initDatabase = async () => {
@@ -8,8 +10,15 @@ export const initDatabase = async () => {
 
     // Crea l'utente admin se non esiste
     await createAdminUser();
+
+    try {
+      // await getRepos();
+      appLogger.info('Dati GitHub recuperati con successo');
+    } catch (error) {
+      appLogger.error(`Errore nel recupero dati GitHub: ${error}`);
+    }
   } catch (error: any) {
-    // console.error('Errore durante la sincronizzazione del database:', error);
+    appLogger.error('Errore durante la sincronizzazione del database:', error);
     throw new Error(
       `Errore durante la sincronizzazione del database: ${error.message}`
     );

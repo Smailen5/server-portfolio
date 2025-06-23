6d9e1b8755ff0991bfcc3473ddfd6ff6fe42caa1
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-06-23 14:55:45 +0200

    fix: 🔧 sostituisce la logica di connessione a MongoDB con Mongoose
    - Rimosso il vecchio sistema di connessione a MongoDB e implementata la connessione tramite Mongoose per una gestione semplificata.
    - Aggiornati i messaggi di log per riflettere le modifiche nella connessione al database.

--- src/config/mongodb.ts
+++ src/config/mongodb.ts
@@ -1,33 +1,13 @@
-
-import { Collection, Db, MongoClient } from 'mongodb';
+import mongoose from 'mongoose';
 import { appLogger } from './appLogger';
 import { env } from './env';
 
-class DbConnection {
-  private mongoClient: MongoClient;
-  static db: Db;
-  static userCollection: Collection;
-
-  constructor() {
-    this.mongoClient = new MongoClient(`${env.dbConnection}`);
+export const connectMongo = async () => {
+  try {
+    await mongoose.connect(`${env.dbConnection}`);
+    appLogger.info('Connessione al database MongoDb stabilita');
+  } catch (err) {
+    appLogger.error('Errore nella connessione al database MongoDb: ', err);
+    throw err;
   }
-
-  async getConnection() {
-    try {
-      await this.mongoClient.connect();
-      const db = this.mongoClient.db('server-portfolio');
-      DbConnection.setInstance(db);
-      appLogger.info('Connessione al database MongoDB stabilita');
-    } catch (err) {
-      appLogger.error('Errore nella connessione al database MongoDB: ', err);
-      throw err;
-    }
-  }
-
-  static setInstance(db: Db) {
-    DbConnection.db = db;
-    DbConnection.userCollection = db.collection('users');
-  }
-}
-
-export default DbConnection;
+};

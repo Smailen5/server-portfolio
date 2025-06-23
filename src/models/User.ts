b2f9fcd06c82b6fbe57e752296e8b927a0a7cc26
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-06-23 14:24:56 +0200

    feat: ✨ aggiunge modello User per la gestione degli utenti in MongoDB
    - Creato il file User.ts con l'interfaccia IUser e il relativo schema Mongoose.
    - Definiti i campi necessari come nome, email, password, ruolo, stato attivo, ultimo accesso, e timestamp di creazione e aggiornamento.

---
+++ src/models/User.ts
@@ -0,0 +1,25 @@
+import mongoose, { Schema, Document } from 'mongoose';
+
+export interface IUser extends Document {
+  name: string;
+  email: string;
+  password: string;
+  role: string;
+  isActive: boolean;
+  lastLogin: Date;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const UserSchema = new Schema({
+  name: { type: String, required: true},
+  email: { type: String, required: true, unique: true},
+  password: { type: String, required: true},
+  role: { type: String, required: true, enum: ['admin', 'user']},
+  isActive: { type: Boolean, default: true},
+  lastLogin: { type: Date, default: Date.now()},
+  createdAt: { type: Date, default: Date.now()},
+  updatedAt: { type: Date, default: Date.now()},
+})
+
+export const User = mongoose.model<IUser>('User', UserSchema)

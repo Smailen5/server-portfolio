641914e937a5bf3ead247acc81a2ec45f52daa8b
Smailen Vargas <126914694+Smailen5@users.noreply.github.com>
2026-07-07 15:01:03 +0200

    fix: rimuove skip del httpLogger in produzione (#149)

--- src/middleware/httpLogger.ts
+++ src/middleware/httpLogger.ts
@@ -5,7 +5,4 @@ const logFormat =
   'Method: :method Url: :url Status: :status Response Time: :response-time ms - Size: :res[content-length]';
 
 // Middleware per il logging delle richieste HTTP
-export const httpLogger = morgan(logFormat, {
-  // Log solo in development
-  skip: (_req, _res) => process.env.NODE_ENV === 'production',
-});
+export const httpLogger = morgan(logFormat);

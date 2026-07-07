601d816eb86a38064b449bf8d5ebd309dc9ba05a
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2026-07-07 11:54:23 +0200

    refactor: adegua loginValid a AppError
    - Sostituisce res.status(400).json(...) con return next(new AppError(...))
    - Allinea il pattern a validateRequest (validatorsRequest.ts) che gia usa AppError

--- src/middleware/validators/validatorsLogin.ts
+++ src/middleware/validators/validatorsLogin.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 import { validationResult } from 'express-validator';
+import { AppError } from '../errorHandler.js';
 import { appLogger } from '../../config/appLogger.js';
 
 export const handleLoginValidation = (
@@ -12,11 +13,8 @@ export const handleLoginValidation = (
     appLogger.warn(
       `Tentativo di login con dati non validi: ${errors.array().map((e) => e.msg).join('; ')}`
     );
-    res.status(400).json({
-      success: false,
-      errors: errors.array(),
-    });
-    return;
+    const errorMessages = errors.array().map((err) => err.msg);
+    return next(new AppError(errorMessages.join(', '), 400));
   }
   next();
 };

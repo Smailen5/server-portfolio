1946136fa9a19ea907fa31efe1f193c81a99bd26
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2026-07-08 15:43:14 +0200

    fix: messaggi generici in syncValidator
    - isLength passa da { min: 20, max: 20 } a { min: 16, max: 64 }
    - Messaggi di errore sostituiti con 'Chiave API non valida'

--- src/middleware/validators/githubValidators.ts
+++ src/middleware/validators/githubValidators.ts
@@ -7,10 +7,8 @@ export const syncValidator = [
     .withMessage('La chiave API è necessaria')
     .isString()
     .withMessage('La chiave API deve essere una stringa')
-    .isLength({ min: 20, max: 20 })
-    .withMessage('La chiave API deve essere di 20 caratteri')
+    .isLength({ min: 16, max: 64 })
+    .withMessage('Chiave API non valida')
     .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{20}$/)
-    .withMessage(
-      'La chiave API deve contenere lettere maiuscole, minuscole e numeri'
-    ),
+    .withMessage('Chiave API non valida'),
 ];

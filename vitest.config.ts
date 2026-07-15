18df83093dfd7c86683cbafa2bfef1625ff0c758
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2026-07-15 14:29:01 +0200

    test: aggiorna configurazione vitest
    - aggiunge coverage alla configurazione vitest.
    - setta coverage a >80% per la copetura dei test.
    - aggiunge configurazione da rimuovere per test non ancora implementati.

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,5 +4,31 @@ export default defineConfig({
   test: {
     environment: "node",
     include: ["src/**/*.{test,spec}.{ts,js}"],
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "html"],
+      reportsDirectory: "./coverage",
+      include: ["src/**/*.ts"],
+      exclude: [
+        "src/**/*.test.ts",
+        "src/**/*.spec.ts",
+        "src/middleware/index.ts",
+        "src/middleware/auth/index.ts",
+        "src/middleware/validators/index.ts",
+        // configurazioni da rimuovere man mano che vengono implementati i test
+        "src/models/**",
+        "src/config/**",
+        "src/index.ts",
+        "src/routes/**",
+        "src/services/**",
+        "src/seeders/**",
+      ],
+      thresholds: {
+        lines: 80,
+        branches: 80,
+        functions: 80,
+        statements: 80,
+      },
+    },
   },
 });

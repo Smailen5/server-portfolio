ec43282c7df25c8252a6d7bb4cf4723c46cd306e
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2026-07-29 16:39:09 +0200

    feat: aggiunge mapper per progetti

---
+++ src/utils/projectMapper.ts
@@ -0,0 +1,13 @@
+import { IProject } from "../models/Projects.js";
+import { ProjectResponse } from "../types/index.js";
+
+export const mapProjectToResponse = (project: IProject): ProjectResponse => ({
+  name: project.name,
+  description: project.description,
+  technologies: project.technologies,
+  images: project.images,
+  repoUrl: project.repoUrl,
+  version: project.version,
+  createdAt: String(project.createdAt),
+  readmeContent: project.readme,
+});
